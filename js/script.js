cff0092c6f36b7fa5eb2b3d5064bfbb8e2057ef5
troxtele <105340164+troxtele@users.noreply.github.com>
2023-05-19 15:16:38 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,3 +20,46 @@ articles.map((article) => {
     arrow.classList.toggle("rotate-arrow");
   });
 });
+
+// Change price
+
+const day = 45;
+
+const priceButtons = [...document.querySelectorAll(".prices button")];
+const dayOne = document.querySelector(".day-one");
+const dayTwo = document.querySelector(".day-two");
+
+priceValues = {
+  0: ["500", "500", "500", "1,000", "1,000", "1,000", "1,000", "500"],
+  1: ["500", "500", "500", "1,000", "1,000", "1,000", "2,500", "1,500"],
+  2: ["2,500", "2,500", "2,500", "5,000", "5,000", "5,000", "5,000", "2,500"],
+  3: [
+    "5,000",
+    "5,000",
+    "5,000",
+    "10,000",
+    "10,000",
+    "10,000",
+    "10,000",
+    "5,000",
+  ],
+};
+
+const prices = [...document.querySelectorAll(".priceVal")];
+
+priceButtons.map((btn, i) => {
+  btn.addEventListener("click", () => {
+    // Change days
+    dayOne.innerHTML = 45;
+    dayTwo.innerHTML = 45;
+
+    // Change prices
+    prices.map((item, j) => {
+      item.innerHTML = priceValues[i][j];
+    });
+
+    // Button outline change
+    priceButtons.map((button) => button.classList.remove("button-outline"));
+    btn.classList.add("button-outline");
+  });
+});
